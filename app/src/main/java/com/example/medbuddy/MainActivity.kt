e37d9d4a8ebf3ef4d2bac7d30eff409c87f04901
CotulbeaAdrian <cotulbeaadrianionut@gmail.com>
2023-01-03 08:24:07 +0200

    Removed unused libs

--- app/src/main/java/com/example/medbuddy/MainActivity.kt
+++ app/src/main/java/com/example/medbuddy/MainActivity.kt
@@ -5,19 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import android.view.View
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.ktx.Firebase
 import android.util.Pair as UtilPair
 
 

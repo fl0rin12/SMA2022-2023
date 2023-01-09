1505ad06244014f97ce43781aaf654b01196b413
CotulbeaAdrian <cotulbeaadrianionut@gmail.com>
2023-01-09 05:43:14 +0200

    Create a help request is done

--- app/src/main/java/com/example/medbuddy/DoctorTreatmentAdapter.kt
+++ app/src/main/java/com/example/medbuddy/DoctorTreatmentAdapter.kt
@@ -12,8 +12,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class TreatmentAdapter(val context: Context, private val treatmentList: ArrayList<Treatment>) :
-    RecyclerView.Adapter<TreatmentAdapter.UserViewHolder>() {
+class DoctorTreatmentAdapter(val context: Context, private val treatmentList: ArrayList<Treatment>) :
+    RecyclerView.Adapter<DoctorTreatmentAdapter.UserViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
